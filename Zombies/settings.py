f0f3efa8ebde3b7120804086ec2c6874b620318f
Tanishq Tiwari <tanishq2149621496@iiitd.ac.in>
2023-08-19 21:14:50 +0530

    dist directory error

--- Zombies/settings.py
+++ Zombies/settings.py
@@ -34,7 +34,7 @@ DEBUG = os.environ.get('DEBUG'),
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = [ '127.0.0.1' , '.vercel.app'  , '.now.sh']
+ALLOWED_HOSTS = [ '127.0.0.1' , '.vercel.app' , '.now.sh']
 
 
 # Application definition
@@ -128,9 +128,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [BASE_DIR/'static',]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Replace 'static' with the name of your static directory
+]
 
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles_build' , 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
