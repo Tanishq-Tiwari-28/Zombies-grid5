a621c5447792682f702143b8fdf9af9250598702
Tanishq Tiwari <tanishq2149621496@iiitd.ac.in>
2023-08-19 12:32:18 +0530

    updated database.py

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
-import sqlite3
+# import sqlite3
 
-# Connect to the SQLite database
-conn = sqlite3.connect('Zombies\db.sqlite3')
-c = conn.cursor()
+# # Connect to the SQLite database
+# conn = sqlite3.connect('Zombies\db.sqlite3')
+# c = conn.cursor()
 
-c.execute('CREATE TABLE IF NOT EXISTS synonyms (drugID INTEGER, synonym TEXT, '
-               'FOREIGN KEY (drugID) REFERENCES drugsList(id))')
-conn.commit()
+# c.execute('CREATE TABLE IF NOT EXISTS synonyms (drugID INTEGER, synonym TEXT, '
+#                'FOREIGN KEY (drugID) REFERENCES drugsList(id))')
+# conn.commit()
