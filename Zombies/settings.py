85f92d2d8c4455cff0871c23616c65dd8ef02eab
Tanishq Tiwari <tanishq2149621496@iiitd.ac.in>
2023-08-19 21:41:10 +0530

    settings.py updated

--- Zombies/settings.py
+++ Zombies/settings.py
@@ -128,9 +128,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Replace 'static' with the name of your static directory
-]
+STATICFILES_DIRS = BASE_DIR/'static'
 
 STATIC_ROOT = BASE_DIR/'staticfiles'
 # Default primary key field type
