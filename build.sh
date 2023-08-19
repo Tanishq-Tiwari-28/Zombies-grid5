f04f9155a8cfdd5a9fdc60ec9305327995251630
Tanishq Tiwari <tanishq2149621496@iiitd.ac.in>
2023-08-19 23:49:28 +0530

    build.sh  updated

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 echo "BUILD STARTS"
 python -m pip install -r requirements.txt
 echo "Installing dependencies done."
-python3.9 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
 echo "Collecting static files done."
 echo "BUILD ENDS"
