221a9651a5adff9075d71a486f516e74cfd702a0
Tanishq Tiwari <tanishq2149621496@iiitd.ac.in>
2023-08-19 21:33:16 +0530

    settings.py updated

--- Zombies/settings.py
+++ Zombies/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Replace 'static' with the name of your static directory
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles_build' , 'static')
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles' , 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
