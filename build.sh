a06f06b05de4d67d548c8006404ce2a8d21ecab3
Tanishq Tiwari <tanishq2149621496@iiitd.ac.in>
2023-08-19 17:29:29 +0530

    build.sh is updated

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
+echo "BUILD STARTS"
+python -m pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
+echo "BUILD ENDS"
